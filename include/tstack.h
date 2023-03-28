f9f8f7b557b69fb8a1b8acce9156ea2014e347a2
rainbowchazer <125148013+rainbowchazer@users.noreply.github.com>
2023-03-28 22:35:54 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,38 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+#include <string>
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+  T arr[size];
+  int top;
+public:
+  Tstack() :top(-1) {};
+  void push(T& value) {
+  if (usfull())
+    throw std::string("Full");
+  else arr[++top] = value;
+  }
+  const T& pop() const {
+    if (isempty())
+      throw std::string("Empty");
+        else return arr[top--];
+  }
+	const T& get() const {
+		if (isempty())
+			throw std::string("Empty");
+		else if (usfull())
+			throw std::string("Full");
+		else return arr[top];
+	}
+	bool isempty() const {
+		return top == -1;
+	}
+	bool isfull() const {
+		return top == size;
+	}
 };
 
 #endif  // INCLUDE_TSTACK_H_
